b0b35b391b652bcd64d9ff953b314e852d876c1e
Angelina <gela-turlova@inbox.ru>
2023-12-04 12:19:37 +0300

    added react lazy, suspense for some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css';
+import { Suspense, lazy } from 'react';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import UsersContainer from './components/Users/UsersContainer';
 import Settings from './components/Settings/Settings';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login/Login';
 import store from './redux/redux-store';
 import { Provider } from 'react-redux';
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
 
 const App = function (props) {
   return (
@@ -21,8 +22,8 @@ const App = function (props) {
         <Navbar />
         <div className='app-wrapper-content'>
           <Routes>
-            <Route path='/dialogs/*' element={<DialogsContainer/>} />
-            <Route path='/profile/:userId?' element={<ProfileContainer />} />
+            <Route path='/dialogs/*' element={<Suspense><DialogsContainer /></Suspense>} />
+            <Route path='/profile/:userId?' element={<Suspense><ProfileContainer /></Suspense>} />
             <Route path='/news/*' element={<News/>} />
             <Route path='/music/*' element={<Music/>} />
             <Route path='/users/*' element={<UsersContainer/>} />
